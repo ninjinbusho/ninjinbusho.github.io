e1170d2d3fe961511e7032c385959ef8c49efec0
tatsui <tatsui.inoue@gmail.com>
2024-12-23 13:26:43 +0900

    Change main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 function init() {
-    var map = L.map('map');       // (1)地図を表示する場所
-    map.setView([35.658825, 139.702904], 5); // (2)地図の中心座標とズームレベル
-    // (3)表示する地図のURLや著作権表記
-     L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
-           attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
-       }).addTo(map);
-     }
+    var map = L.map('map', {
+        crs: L.CRS.Simple
+    });       // (1)地図を表示する場所
+}
+var bounds = [[0,0], [1000,1000]];
+var image = L.imageOverlay('P1360169-1.jpg', bounds).addTo(map);
+map.fitBounds(bounds);
 
 const canvas = document.getElementById("myCanvas");        
     let imagePath = "image.jpg";
